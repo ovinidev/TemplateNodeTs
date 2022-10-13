044bf7b30b9ea6281939f2936565ad3001a01116
vinimedeiros13 <vinimedeiros7@outlook.com>
2022-10-13 08:11:57 -0300

    update project

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express';
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-  return res.status(200).json({ code: 'tudo certo jovem' });
+  return res.status(200).json({ code: 'Sucesso' });
 });
 
 app.listen(8080, () => {
